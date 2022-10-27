87b223065f88fcf9224db0f7b519982b943c3872
aanfadillah <115763475+aanfadillah@users.noreply.github.com>
2022-10-27 20:12:08 +0700

    Add files via upload

---
+++ lingkaran aan.py
@@ -0,0 +1,9 @@
+print('Menghitung luas dan keliling lingakaran')
+print('==============================')
+r = int(input('masukan jari-jari lingkaran: '))
+phi = 3.14
+L = phi * (r * r)
+K = 2*phi*r
+print('Luas lingakaran dengan jari-jari {} adalah {}'.format(r, L))
+print('Keliling lingakaran dengan jari-jari {} adalah {}'.format(r, K))
+
